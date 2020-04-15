d2eac9353cf4f0ff45e5caa7de0c70c03f21a422
aerubianoma <aerubianoma@unal.edu.co>
2020-04-14 22:11:04 -0500

    cambie los datos para que el computador no se les toste como el mio

--- src/prueba.py
+++ src/prueba.py
@@ -12,9 +12,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.calculate_buttom.clicked.connect(self.actualizar)  
     def actualizar(self):
         prueba = Distance_correlation()
-        prueba.x = np.random.uniform(-1,1,10000)
-        prueba.y = np.random.uniform(-1,1,10000)
-        prueba.calculateDistanceCorrelation(10000)
+        prueba.x = np.random.uniform(-1,1,100)
+        prueba.y = np.random.uniform(-1,1,100)
+        prueba.calculateDistanceCorrelation(100)
         self.distance_label.setText(str(prueba.distance_correlation))
         
 if __name__ == "__main__":
